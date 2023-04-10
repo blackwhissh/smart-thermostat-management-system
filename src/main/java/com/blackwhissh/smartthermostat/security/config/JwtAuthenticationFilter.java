93654c882213d618038df2cdae2be1245fdb5037
blackwhissh <blackwhissh@gmail.com>
2023-04-10 02:07:44 -0700

    Added Security

--- src/main/java/com/blackwhissh/smartthermostat/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/blackwhissh/smartthermostat/security/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.blackwhissh.smartthermostat.security.config;
 
+import com.blackwhissh.smartthermostat.security.token.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,33 +22,34 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain)
-            throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail; //TODO Change to username
-
-        //Check
-        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
-
-        //Extract and Validate
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);//TODO Extract userEmail from JWT Token, later username
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); //TODO Change to username
-
-            //Is token still valid?
-            if (jwtService.isTokenValid(jwt, userDetails)){
-                //Update security context
+        userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
@@ -56,11 +58,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 authToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
-                //Update security context
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        //Filter
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
 }
